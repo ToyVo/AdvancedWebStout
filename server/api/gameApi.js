f42137b307fcbdff048ac17379177583ee656f50
Collin Diekvoss <cdiekvoss@gmail.com>
2020-03-26 12:07:24 -0500

    mod2.2 done

--- server/api/gameApi.js
+++ server/api/gameApi.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import games from './games'
 
-export default function newApiRouter (title) {
+export default function ApiRouter () {
   const partialGames = games.map(game => ({
     id: game.id,
     name: game.name,
     year: game.year
-  }));
+  }))
 
   const router = express.Router()
 
@@ -22,7 +22,7 @@ export default function newApiRouter (title) {
       if (singleGame) {
         res.json(singleGame)
       } else {
-        res.status(404).json({"error":"game not found"})
+        res.status(404).json({ error: 'game not found' })
       }
     })
 
