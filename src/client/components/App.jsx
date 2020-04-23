83739d59893972685eee3d3eab4eeea69cf23c34
Collin Diekvoss <cdiekvoss@gmail.com>
2020-04-23 13:13:56 -0500

    modal working

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Axios from 'axios'
 import Banner from './Banner.jsx'
+import GameGrid from './GameGrid.jsx'
+import GameDetailsModal from './GameDetailsModal.jsx'
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      gamesData: []
+      gamesData: [],
+      activeGame: null
     }
+    this.setActiveGame = this.setActiveGame.bind(this)
+  }
 
+  setActiveGame(gameID) {
+    Axios.get(`/api/games/${gameID}`)
+      .then((results) => {
+        this.setState({
+          activeGame: results.data
+        })
+      })
+      .catch((e) => {
+        console.error('error retriving game data')
+        console.error(e.message)
+      })
+  }
+
+  componentDidMount() {
     Axios.get('/api/games')
       .then((results) => {
         this.setState({
@@ -21,12 +40,23 @@ class App extends React.Component {
       })
   }
 
-  render () {
+  render() {
     return (
       <div>
-        <Banner gamesData={this.state.gamesData} title='Game Browser'>
-          Click on a game for more information
-        </Banner>
+        <div className='container'>
+          <div className='row'>
+            <Banner title='Game Browser'>
+              Click on a game for more information
+            </Banner>
+          </div>
+          <GameGrid
+            gamesData={this.state.gamesData}
+            activeGameCallback={this.setActiveGame}
+          />
+        </div>
+        {this.state.activeGame && (
+          <GameDetailsModal game={this.state.activeGame} />
+        )}
       </div>
     )
   }
--- src/client/components/Banner.jsx
+++ src/client/components/Banner.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import GameGrid from './GameGrid.jsx'
 
 class Banner extends React.Component {
   render () {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='pb-2 mt-4 mb-2 border-bottom full-width'>
-            <h1>{this.props.title}</h1>
-            {this.props.children}
-          </div>
-          <GameGrid gamesData={this.props.gamesData}/>
-        </div>
+      <div className='pb-2 mt-4 mb-2 border-bottom full-width'>
+        <h1>{this.props.title}</h1>
+        {this.props.children}
       </div>
     )
   }
@@ -20,8 +14,7 @@ class Banner extends React.Component {
 
 Banner.propTypes = {
   title: PropTypes.string,
-  children: PropTypes.node,
-  gamesData: PropTypes.arrayOf(PropTypes.object).isRequired
+  children: PropTypes.node
 }
 
 export default Banner
