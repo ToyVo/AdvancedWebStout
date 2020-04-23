e4ae575cbfe24da7d6611ff00b2a7e97a48ff34b
Collin Diekvoss <cdiekvoss@gmail.com>
2020-04-23 11:52:50 -0500

    get data from express server

--- src/client/components/GameTile.jsx
+++ src/client/components/GameTile.jsx
@@ -2,24 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class GameTile extends React.Component {
-  render() {
+  render () {
     return (
       <div className='col-xs-12 col-sm-6 col-md-4 col-lg-3'>
         <div className='gameSummary'>
           <a
             className='gameTileLink'
-            href={`data/movies/${this.props.gameData.id}`}
+            href={`/api/games/${this.props.gameData.id}`}
           >
             <span className='summaryTitle'> {this.props.gameData.name}</span>
           </a>
-          <br />
-          <span className='summaryInfo'>
-            {this.props.gameData.designers.join(', ')}
-            <br />
-            {this.props.gameData.artists.join(', ')}
-            <br />
-            {this.props.gameData.publishers.join(', ')}
-          </span>
         </div>
       </div>
     )
