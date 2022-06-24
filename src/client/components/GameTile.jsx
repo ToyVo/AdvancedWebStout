7ae1641757a4f98563a4f1e7cb3841a1822c6806
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:14:13 -0500

    modal working

--- src/client/components/GameTile.jsx
+++ src/client/components/GameTile.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class GameTile extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.gameTileClicked = this.gameTileClicked.bind(this)
+  }
+
+  render() {
     return (
       <div className='col-xs-12 col-sm-6 col-md-4 col-lg-3'>
         <div className='gameSummary'>
           <a
+            onClick={this.gameTileClicked}
             className='gameTileLink'
             href={`/api/games/${this.props.gameData.id}`}
           >
@@ -16,10 +22,16 @@ class GameTile extends React.Component {
       </div>
     )
   }
+
+  gameTileClicked(event) {
+    event.preventDefault()
+    this.props.activeGameCallback(this.props.gameData.id)
+  }
 }
 
 GameTile.propTypes = {
-  gameData: PropTypes.object.isRequired
+  gameData: PropTypes.object.isRequired,
+  activeGameCallback: PropTypes.func.isRequired
 }
 
 export default GameTile
