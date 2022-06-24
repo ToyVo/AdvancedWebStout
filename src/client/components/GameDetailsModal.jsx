50fa01b8368e08e01d4db5d434be17d42b216301
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:14:17 -0500

    use dialog instead of bootstrap modal, using buttons instead of paper for entries

--- src/client/components/GameDetailsModal.jsx
+++ src/client/components/GameDetailsModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Axios from 'axios'
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
+import Transition from '../helpers/Transition.jsx'
 
 export default function GameDetailsModal (props) {
   const [open, setOpen] = useState(true)
@@ -23,29 +24,41 @@ export default function GameDetailsModal (props) {
       })
   }
 
-  return (<Dialog open={open} onClose={handleClose} aria-labelledby='details-dialog-title' scroll='body'>
-    <DialogTitle id='details-dialog-title'>{props.game.name}</DialogTitle>
-    <DialogContent>
-      <ul>
-        <li>Name = {props.game.name}</li>
-        <li>Year = {props.game.year}</li>
-        <li>Rating = {props.game.rating}</li>
-        <li>Min Players = {props.game.minPlayers}</li>
-        <li>Max Players = {props.game.maxPlayers}</li>
-        <li>Min Playtime = {props.game.minPlaytime}</li>
-        <li>Max Playtime = {props.game.maxPlaytime}</li>
-        <li>Min Age = {props.game.minAge}</li>
-        <li>Designers = {props.game.designers.join(', ')}</li>
-        <li>Artists = {props.game.artists.join(', ')}</li>
-        <li>Publishers = {props.game.publishers.join(', ')}</li>
-        <li>ID = {props.game._id}</li>
-      </ul>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={deleteGame} variant='contained' color='secondary'>Delete</Button>
-      <Button onClick={handleClose} variant='outlined' color='primary'>Close</Button>
-    </DialogActions>
-  </Dialog>)
+  return (
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      onClose={handleClose}
+      aria-labelledby='details-dialog-title'
+      scroll='body'
+    >
+      <DialogTitle id='details-dialog-title'>{props.game.name}</DialogTitle>
+      <DialogContent>
+        <ul>
+          <li>Name = {props.game.name}</li>
+          <li>Year = {props.game.year}</li>
+          <li>Rating = {props.game.rating}</li>
+          <li>Min Players = {props.game.minPlayers}</li>
+          <li>Max Players = {props.game.maxPlayers}</li>
+          <li>Min Playtime = {props.game.minPlaytime}</li>
+          <li>Max Playtime = {props.game.maxPlaytime}</li>
+          <li>Min Age = {props.game.minAge}</li>
+          <li>Designers = {props.game.designers.join(', ')}</li>
+          <li>Artists = {props.game.artists.join(', ')}</li>
+          <li>Publishers = {props.game.publishers.join(', ')}</li>
+          <li>ID = {props.game._id}</li>
+        </ul>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={deleteGame} variant='contained' color='secondary'>
+          Delete
+        </Button>
+        <Button onClick={handleClose} variant='outlined' color='primary'>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
 }
 
 GameDetailsModal.propTypes = {
