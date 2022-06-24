c99fe40ba852bda1491d80b75a02f410c621c3cd
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:14:17 -0500

    using material-ui instead of bootstrap

--- src/client/components/GameDetailsModal.jsx
+++ src/client/components/GameDetailsModal.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Axios from 'axios'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
 
 export default function GameDetailsModal (props) {
-  useEffect(() => {
-    $('#boardGameModal').modal().on('hidden.bs.modal', () => {
-      props.onCloseDetailsModal()
-    })
-  })
+  const [open, setOpen] = useState(true)
+
+  const handleClose = () => {
+    setOpen(false)
+    props.onCloseDetailsModal()
+  }
 
   const deleteGame = () => {
     Axios.delete(`/api/games/${props.game._id}`)
       .then(() => {
-        $('#boardGameModal').modal('hide')
+        handleClose()
         props.deleteGame(props.game._id)
       })
       .catch((e) => {
@@ -21,60 +23,29 @@ export default function GameDetailsModal (props) {
       })
   }
 
-  return (<div
-    className='modal fade'
-    id='boardGameModal'
-    tabIndex='-1'
-    role='dialog'
-    aria-labelledby='boardGameModalLabel'
-    aria-hidden='true'
-  >
-    <div className='modal-dialog' role='document'>
-      <div className='modal-content'>
-        <div className='modal-header'>
-          <h5 className='modal-title' id='boardGameModalLabel'>
-            {props.game.name}
-          </h5>
-          <button
-            type='button'
-            className='close'
-            data-dismiss='modal'
-            aria-label='Close'
-          >
-            <span aria-hidden='true'>&times;</span>
-          </button>
-        </div>
-        <div id='boardGameModalBody' className='modal-body'>
-          <ul>
-            <li>Name = {props.game.name}</li>
-            <li>Year = {props.game.year}</li>
-            <li>Rating = {props.game.rating}</li>
-            <li>Min Players = {props.game.minPlayers}</li>
-            <li>Max Players = {props.game.maxPlayers}</li>
-            <li>Min Playtime = {props.game.minPlaytime}</li>
-            <li>Max Playtime = {props.game.maxPlaytime}</li>
-            <li>Min Age = {props.game.minAge}</li>
-            <li>Designers = {props.game.designers.join(', ')}</li>
-            <li>Artists = {props.game.artists.join(', ')}</li>
-            <li>Publishers = {props.game.publishers.join(', ')}</li>
-            <li>ID = {props.game._id}</li>
-          </ul>
-        </div>
-        <div className='modal-footer'>
-          <button className='btn btn-danger' onClick={deleteGame}>
-            Delete
-          </button>
-          <button
-            type='button'
-            className='btn btn-primary'
-            data-dismiss='modal'
-          >
-            Close
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>)
+  return (<Dialog open={open} onClose={handleClose} aria-labelledby='details-dialog-title' scroll='body'>
+    <DialogTitle id='details-dialog-title'>{props.game.name}</DialogTitle>
+    <DialogContent>
+      <ul>
+        <li>Name = {props.game.name}</li>
+        <li>Year = {props.game.year}</li>
+        <li>Rating = {props.game.rating}</li>
+        <li>Min Players = {props.game.minPlayers}</li>
+        <li>Max Players = {props.game.maxPlayers}</li>
+        <li>Min Playtime = {props.game.minPlaytime}</li>
+        <li>Max Playtime = {props.game.maxPlaytime}</li>
+        <li>Min Age = {props.game.minAge}</li>
+        <li>Designers = {props.game.designers.join(', ')}</li>
+        <li>Artists = {props.game.artists.join(', ')}</li>
+        <li>Publishers = {props.game.publishers.join(', ')}</li>
+        <li>ID = {props.game._id}</li>
+      </ul>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={deleteGame} variant='contained' color='secondary'>Delete</Button>
+      <Button onClick={handleClose} variant='outlined' color='primary'>Close</Button>
+    </DialogActions>
+  </Dialog>)
 }
 
 GameDetailsModal.propTypes = {
