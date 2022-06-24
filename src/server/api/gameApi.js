17a946b75c47c468baebeb979e7d838c0dd103f8
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:14:11 -0500

    webpack and ejs

--- src/server/api/gameApi.js
+++ src/server/api/gameApi.js
@@ -1,35 +1,33 @@
 import express from 'express'
 
-// bring in the raw json data, we can import it like this instead of doing 
+// bring in the raw json data, we can import it like this instead of doing
 // fs.readfile, this will bring it in as a javascript object, in this case an array
 import games from './games'
 
-export default function ApiRouter() {
-  const partialGames = games.map(game => ({
-    id: game.id,
-    name: game.name,
-    year: game.year
-  }))
+export const partialGames = games.map((game) => ({
+  id: game.id,
+  name: game.name,
+  year: game.year
+}))
 
+export default function ApiRouter () {
   const router = express.Router()
 
   // get all games, with partial data
-  router.route('/')
-    .get((req, res) => {
-      res.json(partialGames)
-    })
+  router.route('/').get((req, res) => {
+    res.json(partialGames)
+  })
 
   // get all data for a specific game
-  router.route('/:id')
-    .get((req, res) => {
-      const id = req.params.id
-      const singleGame = games.find(game => game.id === id)
-      if (singleGame) {
-        res.json(singleGame)
-      } else {
-        res.status(404).json({ error: 'game not found' })
-      }
-    })
+  router.route('/:id').get((req, res) => {
+    const id = req.params.id
+    const singleGame = games.find((game) => game.id === id)
+    if (singleGame) {
+      res.json(singleGame)
+    } else {
+      res.status(404).json({ error: 'game not found' })
+    }
+  })
 
   return router
 }
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env, argv) => {
+  const config = {
+    entry: {
+      app: path.join(__dirname, 'src', 'client', 'index.js')
+    },
+
+    // output bundle
+    output: {
+      filename: '[name].bundle.js',
+      path: path.join(__dirname, 'public')
+    },
+
+    // rules
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-async-to-generator']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'client', 'index.html'),
+        filename: path.join(__dirname, 'public', 'index.html')
+      })
+    ]
+  }
+
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map'
+    config.devServer = {
+      contentBase: path.join(__dirname, 'public'),
+      compress: true,
+      port: 9000
+    }
+  }
+
+  if (argv.mode === 'production') {
+    config.module.rules[0].use.options.presets.push('minify')
+  }
+
+  return config
+}
